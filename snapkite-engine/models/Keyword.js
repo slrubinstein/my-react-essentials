159c2569a595beab215376da4faee1deeb580ea3
Steve Rubinstein <srubinstein@theladders.com>
2016-04-01 10:45:24 -0400

    redo project directory structure, update startup.sh

--- snapkite-engine/models/Keyword.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var COLLECTION_NAME = 'keyword';
-
-var keywordSchema = new mongoose.Schema({
-  data: {type: mongoose.Schema.Types.Mixed, required: true}
-}, { collection: COLLECTION_NAME });
-
-module.exports = mongoose.model('Keyword', keywordSchema);
--- startup.sh
+++ startup.sh
@@ -1,11 +1,17 @@
 #!/bin/bash 
+echo ">>> Creating resources directory"
+mkdir ../resources
+echo ">>> Cloning Snapkite Engine"
+git clone https://github.com/Snapkite/snapkite-engine.git ../resources/snapkite-engine
+echo ">>> Moving config.json to Snapkite Engine"
+mv config.json ../resources/snapkite-engine
 echo ">>> Cloning Snapkite filter repos"
-git clone https://github.com/Snapkite/snapkite-filter-is-possibly-sensitive.git snapkite-engine/filters/snapkite-filter-is-possibly-sensitive/
-git clone https://github.com/Snapkite/snapkite-filter-has-mobile-photo snapkite-engine/filters/snapkite-filter-has-mobile-photo/
-git clone https://github.com/Snapkite/snapkite-filter-has-text snapkite-engine/filters/snapkite-filter-has-text/
-git clone https://github.com/Snapkite/snapkite-filter-is-retweet snapkite-engine/filters/snapkite-filter-is-retweet/
+git clone https://github.com/Snapkite/snapkite-filter-is-possibly-sensitive.git ../resources/snapkite-engine/filters/snapkite-filter-is-possibly-sensitive/
+git clone https://github.com/Snapkite/snapkite-filter-has-mobile-photo ../resources/snapkite-engine/filters/snapkite-filter-has-mobile-photo/
+git clone https://github.com/Snapkite/snapkite-filter-has-text ../resources/snapkite-engine/filters/snapkite-filter-has-text/
+git clone https://github.com/Snapkite/snapkite-filter-is-retweet ../resources/snapkite-engine/filters/snapkite-filter-is-retweet/
 echo ">>> Copying Snapkite filter config files"
-cp snapkite-engine/filters/snapkite-filter-has-mobile-photo/example.config.json snapkite-engine/filters/snapkite-filter-has-mobile-photo/config.json
-cp snapkite-engine/filters/snapkite-filter-has-text/example.config.json snapkite-engine/filters/snapkite-filter-has-text/config.json
-cp snapkite-engine/filters/snapkite-filter-is-possibly-sensitive/example.config.json snapkite-engine/filters/snapkite-filter-is-possibly-sensitive/config.json
-cp snapkite-engine/filters/snapkite-filter-is-retweet/example.config.json snapkite-engine/filters/snapkite-filter-is-retweet/config.json
+cp ../resources/snapkite-engine/filters/snapkite-filter-has-mobile-photo/example.config.json ../resources/snapkite-engine/filters/snapkite-filter-has-mobile-photo/config.json
+cp ../resources/snapkite-engine/filters/snapkite-filter-has-text/example.config.json ../resources/snapkite-engine/filters/snapkite-filter-has-text/config.json
+cp ../resources/snapkite-engine/filters/snapkite-filter-is-possibly-sensitive/example.config.json ../resources/snapkite-engine/filters/snapkite-filter-is-possibly-sensitive/config.json
+cp ../resources/snapkite-engine/filters/snapkite-filter-is-retweet/example.config.json ../resources/snapkite-engine/filters/snapkite-filter-is-retweet/config.json
