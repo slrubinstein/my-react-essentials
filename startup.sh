f4664f62eb3824d4a800e4139b30b5df4237f92d
Steve Rubinstein <steve@steveruteinsMBP.home>
2016-03-31 22:31:57 -0400

    update startup script

--- startup.sh
+++ startup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash 
+echo ">>> Cloning Snapkite filter repos"
 git clone https://github.com/Snapkite/snapkite-filter-is-possibly-sensitive.git snapkite-engine/filters/snapkite-filter-is-possibly-sensitive/
 git clone https://github.com/Snapkite/snapkite-filter-has-mobile-photo snapkite-engine/filters/snapkite-filter-has-mobile-photo/
 git clone https://github.com/Snapkite/snapkite-filter-has-text snapkite-engine/filters/snapkite-filter-has-text/
 git clone https://github.com/Snapkite/snapkite-filter-is-retweet snapkite-engine/filters/snapkite-filter-is-retweet/
-
+echo ">>> Copying Snapkite filter config files"
 cp snapkite-engine/filters/snapkite-filter-has-mobile-photo/example.config.json snapkite-engine/filters/snapkite-filter-has-mobile-photo/config.json
 cp snapkite-engine/filters/snapkite-filter-has-text/example.config.json snapkite-engine/filters/snapkite-filter-has-text/config.json
 cp snapkite-engine/filters/snapkite-filter-is-possibly-sensitive/example.config.json snapkite-engine/filters/snapkite-filter-is-possibly-sensitive/config.json
