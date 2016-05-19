1fb3fc7fd460c9a01a5d994c5094124cc1b4eb28
Steve Rubinstein <srubinstein@theladders.com>
2016-05-19 09:10:26 -0400

    add store, action creator, and dispatcher for tweets

---
+++ snapterest/source/actions/TweetActionCreators.js
@@ -0,0 +1,13 @@
+var AppDispatcher = require('../dispatcher/AppDispatcher');
+
+function receiveTweet(tweet) {
+    var action = {
+        type: 'receive_tweet',
+        tweet: tweet
+    };
+    AppDispatcher.dispatch(action);
+}
+
+module.exports = {
+    receiveTweet: receiveTweet
+};
